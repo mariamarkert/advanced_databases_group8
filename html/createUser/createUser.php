940f5002d27e6df710bfc75959e94f5f40d3d900
mariamarkert <mmarkert@nd.edu>
2023-05-07 01:43:49 -0400

    updates

--- html/createUser/createUser.php
+++ html/createUser/createUser.php
@@ -3,15 +3,57 @@
 	include "../connect/connect.php";
 
 	
-	$query = "INSERT INTO users (user_id, username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
-VALUES 
-(126, '$username', '$password', '$first-name', '$last-name', TO_DATE('1995-05-04', 'yyyy-mm-dd'), '$height', '$weight', NULL, '$pregnancies')";
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Validate inputs
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $first_name = htmlspecialchars($_POST['first-name']);
+    $last_name = htmlspecialchars($_POST['last-name']);
+    $birthday = htmlspecialchars($_POST['birthday']);
+    $height = htmlspecialchars($_POST['height']);
+    $weight = htmlspecialchars($_POST['weight']);
+    $pregnancies = htmlspecialchars($_POST['pregnancies']);
 
-	$stmt = oci_parse($conn, $query);		
-	oci_execute($stmt);
+    // Prepare the query
+    $query = "INSERT INTO users (user_id, username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
+              VALUES (:user_id, :username, :password, :first_name, :last_name, TO_DATE(:birthday, 'yyyy-mm-dd'), :height, :weight, NULL, :pregnancies)";
+    $stmt = oci_parse($conn, $query);
+
+    // Bind the values to the prepared statement
+    $user_id = uniqid(); // Generate a unique ID for the user
+    oci_bind_by_name($stmt, ':user_id', $user_id);
+    oci_bind_by_name($stmt, ':username', $username);
+    oci_bind_by_name($stmt, ':password', $password);
+    oci_bind_by_name($stmt, ':first_name', $first_name);
+    oci_bind_by_name($stmt, ':last_name', $last_name);
+    oci_bind_by_name($stmt, ':birthday', $birthday);
+    oci_bind_by_name($stmt, ':height', $height);
+    oci_bind_by_name($stmt, ':weight', $weight);
+    oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
+
+    // Execute the prepared statement
+    if (oci_execute($stmt)) {
+        //echo 'User created successfully.';
+		header("Location: ../home/");
+		oci_free_statement($stmt);
+    	oci_close($conn);
+    	exit();
+    } else {
+        $error = oci_error($stmt);
+        //header("Location: ../createUser/?Error creating user");
+		oci_free_statement($stmt);
+    	oci_close($conn);
+		exit();
+    }
+
+    // Clean up
+    
+}
+?>
+
 			
-    header("Location: ../home/");
-    exit();
+    
 
 ?>
 
--- html/myflow/myflow.php
+++ html/myflow/myflow.php
@@ -1,11 +1,9 @@
 <?php
-
-    function predict_next(){
-        include "../connect/connect.php";
+    include "../connect/connect.php";
         // Query to retrieve the last 6 cycle lengths for user 125
-        $query = "SELECT cycle_length, period_start FROM (SELECT cycle_length, period_start FROM cycle WHERE user_id = '125' ORDER BY period_start DESC) WHERE ROWNUM <= 6";
-        $stid = oci_parse($conn, $query);
-        oci_execute($stid);
+    $query = "SELECT cycle_length, period_start FROM (SELECT cycle_length, period_start FROM cycle WHERE user_id = '125' ORDER BY period_start DESC) WHERE ROWNUM <= 6";
+    $stid = oci_parse($conn, $query);
+    oci_execute($stid);
 
         // Store the lengths in an array
         $lengths = array();
@@ -34,9 +32,13 @@
 
         // Close the DB connection
         oci_free_statement($stid);
-        $last_start_query = "select * from (select period_start from cycle where user_id=125 order by user_cycle_id DESC) where rownum=1";
+        $last_start_query = "select * from (select * from cycle where user_id=125 order by user_cycle_id DESC) where rownum<=2";
         $stid = oci_parse($conn, $query);
         oci_execute($stid);
+        $lengths = array();
+        while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
+            $lengths[] = $row['CYCLE_LENGTH'];
+        }
         $row = oci_fetch_array($stid);
 
         $last_start = $row['period_start'];
