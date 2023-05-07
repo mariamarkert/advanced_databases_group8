424ded215c231d8ce7c77345f1546f4516a6cceb
mariamarkert <mmarkert@nd.edu>
2023-05-07 14:25:36 -0400

    upate

--- html/templates/newcal.php
+++ html/templates/newcal.php
@@ -22,41 +22,47 @@
 
 		// Get the given date
         include "../home/predict.php";
-		$given_date = predict_next();
+$given_date = predict_next();
 
-		// Get the first day of the given date's month
-		$first_day = date('N', strtotime(date('Y-m-01', strtotime($given_date))));
+// Get the month and year from the given date
+$month = date('n', strtotime($given_date));
+$year = date('Y', strtotime($given_date));
 
-		// Get the number of days in the given date's month
-		$num_days = date('t', strtotime($given_date));
+// Get the first day of the given month
+$first_day = date('N', strtotime($year . '-' . $month . '-01'));
 
-		// Calculate the offset based on the first day of the month
-		$offset = ($first_day) % 7;
+// Get the number of days in the given month
+$num_days = date('t', strtotime($year . '-' . $month . '-01'));
 
-		// Loop through each day in the given date's month
-		for ($day = 1; $day <= $num_days; $day++) {
-		    // Determine if this day is within the next 7 days of the given date
-		    $given_day = strtotime($given_date);
-		    $current_day = strtotime("May $day 2023");
-		    $is_next_7_days = false;
-		    if ($current_day >= $given_day && $current_day <= strtotime("+7 days", $given_day)) {
-		        $is_next_7_days = true;
-		    }
+// Calculate the offset based on the first day of the month
+$offset = ($first_day - 1) % 7;
 
-		    // Determine the grid column for this day
-		    $col = ($day + $offset) % 7;
-		    if ($col == 0) {
-		        $col = 7;
-		    }
+// Loop through each day in the given month
+for ($day = 1; $day <= $num_days; $day++) {
+    // Determine if this day is within the next 7 days of the given date
+    $given_day = strtotime($given_date);
+    $current_day = strtotime("$year-$month-$day");
+    $is_next_7_days = false;
+    if ($current_day >= $given_day && $current_day <= strtotime("+7 days", $given_day)) {
+        $is_next_7_days = true;
+    }
 
-		    // Generate the button HTML
-		    echo '<div class="number-wrapper';
-		    if ($is_next_7_days) {
-		        echo ' red';
-		    }
-		    echo '" style="grid-column: ' . $col . '">';
-		    echo $day;
-		    echo '</div>';
+    // Determine the grid column for this day
+    $col = ($day + $offset) % 7;
+    if ($col == 0) {
+        $col = 7;
+    }
+
+    // Generate the button HTML
+    echo '<div class="number-wrapper';
+    if ($is_next_7_days) {
+        echo ' red';
+    }
+    echo '" style="grid-column: ' . $col . '">';
+    echo $day;
+      echo '</div>';
+
+
 		}
 		?>
 	</div>
