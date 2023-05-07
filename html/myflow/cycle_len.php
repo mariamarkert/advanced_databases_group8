0b21ea033d3d9abca674956ec2e6ed634eed94d5
mariamarkert <mmarkert@nd.edu>
2023-05-07 14:43:24 -0400

    upate

--- html/myflow/cycle_len.php
+++ html/myflow/cycle_len.php
@@ -5,15 +5,15 @@
         // Close the DB connection
         
         $query = "select * from (select * from cycle where user_id='$user_id' order by user_cycle_id DESC) where rownum<=5";
-        echo $query;
+        
         $stid = oci_parse($conn, $query);
         oci_execute($stid);
         $lengths = array();
         while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
             $lengths[] = $row['CYCLE_LENGTH'];
         }
-        print_r($lengths);
-        echo $lengths[2];
+        
+        echo $lengths[1];
         
 
         oci_free_statement($stid);
