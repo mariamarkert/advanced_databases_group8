a5b30b293889a4ae39fad03d8ede930b605e1184
mariamarkert <mmarkert@nd.edu>
2023-05-07 13:18:27 -0400

    upate

--- sql/trig2.sql
+++ sql/trig2.sql
@@ -9,15 +9,20 @@ CREATE SEQUENCE cycle_id_seq
   NOCACHE
   NOCYCLE;
 /
-CREATE SEQUENCE user_cycle_id_seq;
-/
+
+
 CREATE OR REPLACE TRIGGER cycle_id_trigger
 BEFORE INSERT ON cycle
 FOR EACH ROW
+DECLARE
+   last_user_cycle_id NUMBER;
 BEGIN
-    :new.cycle_id := cycle_id_seq.nextval;
-    :new.user_cycle_id := user_cycle_id_seq.nextval;
-    :new.cycle_length := NULL;
+   SELECT NVL(MAX(user_cycle_id), 0) INTO last_user_cycle_id
+   FROM cycle
+   WHERE user_id = :new.user_id;
+   :new.user_cycle_id := last_user_cycle_id + 1;
+   :new.cycle_id := cycle_id_seq.nextval;
+   :new.cycle_length := NULL;
 END;
 /
 CREATE OR REPLACE TRIGGER update_cycle_length
@@ -39,33 +44,10 @@ BEGIN
     END IF;
 END;
 
-CREATE OR REPLACE TRIGGER cycle_id_trigger
-BEFORE INSERT ON cycle
-FOR EACH ROW
-DECLARE
-    last_user_cycle_id NUMBER;
-BEGIN
-    -- Get the user_id of the current cycle
-    DECLARE
-      current_user_id NUMBER;
-    BEGIN
-      SELECT user_id INTO current_user_id FROM users WHERE user_name = :new.user_name;
-    EXCEPTION
-      WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20001, 'User not found');
-    END;
 
-    -- Get the user_cycle_id of the last cycle for this user
-    SELECT user_cycle_id INTO last_user_cycle_id
-    FROM cycle
-    WHERE user_id = current_user_id
-    ORDER BY period_end DESC
-    FETCH FIRST 1 ROW ONLY;
-  
-    -- Increment the last user_cycle_id and assign it to the new cycle
-    :new.user_cycle_id := COALESCE(last_user_cycle_id, 0) + 1;
-END;
 /
+
+
 
 
 
