7faea587325a04199c58eb33d98fd4b53d9f3810
mariamarkert <mmarkert@nd.edu>
2023-05-07 11:15:15 -0400

    upate

--- html/createUser/createUser.php
+++ html/createUser/createUser.php
@@ -24,24 +24,45 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 	
     
-	$user_id = 127;
+	$user_id = 128;
     // Prepare the query
     $query = "INSERT INTO users (user_id, username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
-              VALUES ($user_id, $username, $password, $first_name, $last_name, NULL, $height, $weight, NULL, $pregnancies)";
-    $stmt = oci_parse($conn, $query);
-	echo $query;
+          VALUES (:user_id, :username, :password, :first_name, :last_name, TO_DATE(:birthday, 'yyyy-mm-dd'), :height, :weight, NULL, :pregnancies)";
+$stmt = oci_parse($conn, $query);
+echo $query;
 
-    // Bind the values to the prepared statement
-    $user_id = uniqid(); // Generate a unique ID for the user
-    oci_bind_by_name($stmt, ':user_id', $user_id);
-    oci_bind_by_name($stmt, ':username', $username);
-    oci_bind_by_name($stmt, ':password', $password);
-    oci_bind_by_name($stmt, ':first_name', $first_name);
-    oci_bind_by_name($stmt, ':last_name', $last_name);
-    oci_bind_by_name($stmt, ':birthday', $birthday);
-    oci_bind_by_name($stmt, ':height', $height);
-    oci_bind_by_name($stmt, ':weight', $weight);
-    oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
+// Bind the values to the prepared statement
+oci_bind_by_name($stmt, ':user_id', $user_id);
+oci_bind_by_name($stmt, ':username', $username);
+oci_bind_by_name($stmt, ':password', $password);
+oci_bind_by_name($stmt, ':first_name', $first_name);
+oci_bind_by_name($stmt, ':last_name', $last_name);
+oci_bind_by_name($stmt, ':birthday', $birthday);
+oci_bind_by_name($stmt, ':height', $height);
+oci_bind_by_name($stmt, ':weight', $weight);
+oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
+
+// Execute the prepared statement
+if (oci_execute($stmt)) {
+    echo 'User created successfully.';
+    oci_free_statement($stmt);
+    oci_close($conn);
+    //exit();
+} else {
+    $error = oci_error($stmt);
+    echo 'Error creating user: ' . $error['message'];
+    oci_free_statement($stmt);
+    oci_close($conn);
+    exit();
+}
+Note that I have removed the line $user_id = 127 and replaced it with $user_id = uniqid(), since you want to generate a unique ID for each user. Additionally, I have removed the placeholders in the query string and replaced them with named parameters (e.g. :user_id) that are then bound to the actual values using the oci_bind_by_name() function.
+
+
+
+
+
+
+
 
     // Execute the prepared statement
     if (oci_execute($stmt)) {
