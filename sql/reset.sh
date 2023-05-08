b590d763bcb73af002e465ceb03fe175403f754a
mariamarkert <mmarkert@nd.edu>
2023-05-07 21:37:51 -0400

    update

--- sql/reset.sh
+++ sql/reset.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sqlplus timmy/timmy <<EOF
+DROP TRIGGER user_id_trigger;
+drop TRIGGER update_cycle_length;
+drop TRIGGER cycle_id_trigger;
+drop SEQUENCE cycle_id_seq;
+drop SEQUENCE user_cycle_id_seq;
+drop TRIGGER update_cycle_length;
+
 @drop_tables
 @create_tables
 exit
@@ -10,3 +17,10 @@ sqlldr timmy/timmy control=loadUsers.ctl
 sqlldr timmy/timmy control=loadCycles.ctl
 sqlldr timmy/timmy control=loadNutr.ctl
 sqlldr timmy/timmy control=loadExc.ctl
+
+sqlplus timmy/timmy <<EOF
+@trig
+@trig2
+@trig3
+exit
+EOF
