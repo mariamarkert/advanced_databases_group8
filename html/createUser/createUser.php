f4b94acf1fcdb7bfed9af71635f8489594ca956f
mariamarkert <mmarkert@nd.edu>
2023-05-07 22:49:08 -0400

    update

--- html/createUser/createUser.php
+++ html/createUser/createUser.php
@@ -1,63 +1,73 @@
 <?php 
-	session_start(); 
-	include "../connect/connect.php";
-
-	
-// Check if the form was submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Validate inputs
-	echo $_POST['username'];
-    $username = htmlspecialchars($_POST['username']);
-    $password = htmlspecialchars($_POST['password']);
-    $first_name = htmlspecialchars($_POST['first-name']);
-    $last_name = htmlspecialchars($_POST['last-name']);
-    $birthday = htmlspecialchars($_POST['birthday']);
-	if (is_numeric($_POST['height']) && is_numeric($_POST['weight']) && is_numeric($_POST['pregnancies']) ) {
-		// height and weight are numeric, proceed with insertion
-		$height = (int)$_POST['height'];
-		$weight = (int)$_POST['weight'];
-		$pregnancies = (int)$_POST['pregnancies'];
-	} else {
-		// height and/or weight are not numeric, handle error
-		echo "Error: Height and weight must be numeric values.";
-		exit();
-	}
-	
+	session_start();
+    include "../connect/connect.php";
     
-	$user_id = 128;
-    // Prepare the query
-    $query = "INSERT INTO users (user_id, username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
-          VALUES (:user_id, :username, :password, :first_name, :last_name, TO_DATE(:birthday, 'yyyy-mm-dd'), :height, :weight, NULL, :pregnancies)";
-$stmt = oci_parse($conn, $query);
-echo $query;
-
-// Bind the values to the prepared statement
-oci_bind_by_name($stmt, ':user_id', $user_id);
-oci_bind_by_name($stmt, ':username', $username);
-oci_bind_by_name($stmt, ':password', $password);
-oci_bind_by_name($stmt, ':first_name', $first_name);
-oci_bind_by_name($stmt, ':last_name', $last_name);
-oci_bind_by_name($stmt, ':birthday', $birthday);
-oci_bind_by_name($stmt, ':height', $height);
-oci_bind_by_name($stmt, ':weight', $weight);
-oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
-
-// Execute the prepared statement
-if (oci_execute($stmt)) {
-    echo 'User created successfully.';
-    oci_free_statement($stmt);
-    oci_close($conn);
-    header("Location: ../home/");
-    exit();
-} else {
-    $error = oci_error($stmt);
-    echo 'Error creating user: ' . $error['message'];
-    oci_free_statement($stmt);
-    oci_close($conn);
-    header("Location: ../createUser");
-    exit();
-}
-}
+    // Check if the form was submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Validate inputs
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+        $first_name = htmlspecialchars($_POST['first-name']);
+        $last_name = htmlspecialchars($_POST['last-name']);
+        $birthday = htmlspecialchars($_POST['birthday']);
+        if (is_numeric($_POST['height']) && is_numeric($_POST['weight']) && is_numeric($_POST['pregnancies'])) {
+            // height and weight are numeric, proceed with insertion
+            $height = (int)$_POST['height'];
+            $weight = (int)$_POST['weight'];
+            $pregnancies = (int)$_POST['pregnancies'];
+        } else {
+            // height and/or weight are not numeric, handle error
+            echo "Error: Height and weight must be numeric values.";
+            exit();
+        }
+    
+        // Check if the username already exists in the database
+        $query = "SELECT COUNT(*) FROM users WHERE username = :username";
+        $stmt = oci_parse($conn, $query);
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_execute($stmt);
+        $count = oci_fetch_array($stmt)[0];
+        if ($count > 0) {
+            // Username already exists, handle error
+            echo "Error: Username already exists.";
+            exit();
+        }
+    
+        $user_id = 128;
+        // Prepare the query
+        $query = "INSERT INTO users (username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
+              VALUES (:username, :password, :first_name, :last_name, TO_DATE(:birthday, 'yyyy-mm-dd'), :height, :weight, NULL, :pregnancies)";
+        $stmt = oci_parse($conn, $query);
+    
+        // Bind the values to the prepared statement
+        oci_bind_by_name($stmt, ':user_id', $user_id);
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_bind_by_name($stmt, ':password', $password);
+        oci_bind_by_name($stmt, ':first_name', $first_name);
+        oci_bind_by_name($stmt, ':last_name', $last_name);
+        oci_bind_by_name($stmt, ':birthday', $birthday);
+        oci_bind_by_name($stmt, ':height', $height);
+        oci_bind_by_name($stmt, ':weight', $weight);
+        oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
+    
+        // Execute the prepared statement
+        if (oci_execute($stmt)) {
+            $_SESSION["userid"] = $user_id;
+            echo 'User created successfully.';
+            oci_free_statement($stmt);
+            oci_close($conn);
+            header("Location: ../home/");
+            exit();
+        } else {
+            $error = oci_error($stmt);
+            echo 'Error creating user: ' . $error['message'];
+            oci_free_statement($stmt);
+            oci_close($conn);
+            header("Location: ../createUser");
+            exit();
+        }
+    }
+    
 ?>
 
 			
