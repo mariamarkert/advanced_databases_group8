f7bedb651f078b23f061746e4e83f2db749960b2
mariamarkert <mmarkert@nd.edu>
2023-05-07 23:01:42 -0400

    update

--- html/createUser/createUser.php
+++ html/createUser/createUser.php
@@ -37,39 +37,39 @@
         $query = "INSERT INTO users (username, upassword, first_name, last_name, birthday, uheight, uweight, bmi, num_preg)
           VALUES (:username, :password, :first_name, :last_name, TO_DATE(:birthday, 'yyyy-mm-dd'), :height, :weight, NULL, :pregnancies)
           RETURNING user_id INTO :user_id";
-$stmt = oci_parse($conn, $query);
+        $stmt = oci_parse($conn, $query);
 
-// Bind the values to the prepared statement
-oci_bind_by_name($stmt, ':username', $username);
-oci_bind_by_name($stmt, ':password', $password);
-oci_bind_by_name($stmt, ':first_name', $first_name);
-oci_bind_by_name($stmt, ':last_name', $last_name);
-oci_bind_by_name($stmt, ':birthday', $birthday);
-oci_bind_by_name($stmt, ':height', $height);
-oci_bind_by_name($stmt, ':weight', $weight);
-oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
-oci_bind_by_name($stmt, ':user_id', $user_id, -1, OCI_B_INT);
-
-// Execute the prepared statement
-if (oci_execute($stmt)) {
-    // Retrieve the generated user_id value
-    oci_fetch($stmt);
-    $user_id = $user_id;
-    $_SESSION['userid'] = $user_id;
-    echo 'User created successfully.';
-    oci_free_statement($stmt);
-    oci_close($conn);
-    header("Location: ../home/");
-    exit();
-} else {
-    $error = oci_error($stmt);
-    echo 'Error creating user: ' . $error['message'];
-    oci_free_statement($stmt);
-    oci_close($conn);
-    header("Location: ../createUser");
-    exit();
-}
-
+        // Bind the values to the prepared statement
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_bind_by_name($stmt, ':password', $password);
+        oci_bind_by_name($stmt, ':first_name', $first_name);
+        oci_bind_by_name($stmt, ':last_name', $last_name);
+        oci_bind_by_name($stmt, ':birthday', $birthday);
+        oci_bind_by_name($stmt, ':height', $height);
+        oci_bind_by_name($stmt, ':weight', $weight);
+        oci_bind_by_name($stmt, ':pregnancies', $pregnancies);
+        oci_bind_by_name($stmt, ':user_id', $user_id, -1, OCI_B_INT);
+/*
+        // Execute the prepared statement
+        if (oci_execute($stmt)) {
+            // Retrieve the generated user_id value
+            oci_fetch($stmt);
+            $user_id = $user_id;
+            $_SESSION['userid'] = $user_id;
+            echo 'User created successfully.';
+            oci_free_statement($stmt);
+            oci_close($conn);
+            header("Location: ../home/");
+            exit();
+        } else {
+            $error = oci_error($stmt);
+            echo 'Error creating user: ' . $error['message'];
+            oci_free_statement($stmt);
+            oci_close($conn);
+            header("Location: ../createUser");
+            exit();
+        }
+*/
     }
     
 ?>
